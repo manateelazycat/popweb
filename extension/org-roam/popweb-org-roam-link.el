8dab5e2bff3f11f0a6484c25c6c23f8614cfeb36
czqhurricnae <czqhurricane@gmail.com>
2022-01-25 23:06:32 +0800

    Update popweb-org-roam-link.el
    fixed package file header

--- extension/org-roam/popweb-org-roam-link.el
+++ extension/org-roam/popweb-org-roam-link.el
@@ -1,14 +1,71 @@
 ;;; popweb-org-roam-link.el --- Org-Roam ID link and footnote link previewer
+;; Filename: popweb-org-roam-link.el
+;; Description: Org-Roam ID link and footnote link previewer
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2021, Andy Stewart, all rights reserved.
+;; Created: 2022-01-25-23:00:00
+;; Version: 0.1
+;; URL: https://www.github.org/manateelazycat/popweb-dict
+;; Keywords:
+;; Compatibility: GNU Emacs 29.0.50
 ;;
-;; Author: c <c@MacBook-Pro.local>
-;; Copyright © 2022, c, all rights reserved.
-;; Created: 25 January 2022
+;; Features that might be required by this library:
+;; Package-Requires: ((org) (org-transclusion "1.2.0"))
 ;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
 ;;; Commentary:
 ;;
+;; Org-Roam ID link and footnote link previewer
+;;
+
+;;; Installation:
+;;
+;; Put popweb-org-roam-link.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'popweb-org-roam-link)
+;;
+;; No need more.
+
+;;; Customize:
 ;;
 ;;
-;;; Code:
+
+;;; Change log:
+;;
+;;
+
+;;; Acknowledgements:
+;;
+
+;;; TODO
+;;
+
 
 (require 'ox)
 (require 'org-transclusion)
