84379d390dcb6c8defdd52a17b83378702136d93
czqhurricnae <czqhurricane@gmail.com>
2022-01-25 23:57:43 +0800

    Update popweb-org-roam-link.py
    support customize width and height scale

--- extension/org-roam/popweb-org-roam-link.py
+++ extension/org-roam/popweb-org-roam-link.py
@@ -4,22 +4,20 @@ from PyQt5.QtCore import QUrl, QTimer
 import os
 
 
-def adjust_org_roam_link_window(popweb, web_window, window_x, window_y,
-                                x_offset, y_offset, frame_x, frame_y, frame_w,
-                                frame_h, show_window, new_html):
-    if show_window:
-        web_window.show()
-
-
 def pop_org_roam_link_window(popweb, module_name, index_file, x, y, x_offset,
                              y_offset, frame_x, frame_y, frame_w, frame_h,
-                             show_window, new_html, html_string):
+                             width_scale, height_scale, show_window, new_html,
+                             html_string):
     web_window = popweb.get_web_window(module_name)
-    web_window.loading_js_code = ""
-    if new_html == True:
-        web_window.webview.setHtml(html_string, QUrl(index_file))
-
-    QTimer().singleShot(
-        100, lambda: adjust_org_roam_link_window(
-            popweb, web_window, x + x_offset, y + y_offset, x_offset, y_offset,
-            frame_x, frame_y, frame_w, frame_h, show_window, new_html))
+    window_width = frame_w * width_scale
+    window_height = frame_h * height_scale
+    window_x, window_y = popweb.adjust_render_pos(x + x_offset, y + y_offset,
+                                                  x_offset, y_offset,
+                                                  window_width, window_height,
+                                                  frame_x, frame_y, frame_w,
+                                                  frame_h)
+    web_window.webview.setHtml(html_string, QUrl(index_file))
+    web_window.update_theme_mode()
+    web_window.resize(int(window_width), int(window_height))
+    web_window.move(int(window_x), int(window_y))
+    web_window.show()
