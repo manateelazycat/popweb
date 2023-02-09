c08b4dd33ae44510a00bd699ac9849b6e9754811
czqhurricnae <czqhurricane@gmail.com>
2023-02-09 13:12:32 +0800

    Update popweb-dict.py

--- extension/dict/popweb-dict.py
+++ extension/dict/popweb-dict.py
@@ -21,17 +21,24 @@
 
 from PyQt6.QtCore import QUrl
 
-def pop_translate_window(popweb, module_name, x, y, x_offset, y_offset, frame_x, frame_y, frame_w, frame_h, width_scale, height_scale, url, loading_js_code, js_file_code="", js_file_code_args=None):
+def pop_translate_window(popweb, module_name, x, y, x_offset, y_offset, frame_x, frame_y, frame_w, frame_h, width_scale, height_scale, url, loading_js_code, js_file="", js_file_code_args=None):
     web_window = popweb.get_web_window(module_name)
     window_width = frame_w * width_scale
     window_height = frame_h * height_scale
     window_x, window_y = popweb.adjust_render_pos(x + x_offset, y + y_offset, x_offset, y_offset, window_width, window_height, frame_x, frame_y, frame_w, frame_h)
 
     web_window.loading_js_code = loading_js_code
-    web_window.js_file_code = js_file_code
+    web_window.js_file_code = read_js_content(js_file) or ""
     web_window.js_file_code_args = js_file_code_args
     web_window.webview.load(QUrl(url))
     web_window.update_theme_mode()
     web_window.resize(int(window_width), int(window_height))
     web_window.move(int(window_x), int(window_y))
     web_window.popup()
+
+def read_js_content(js_file):
+    ''' Read content of JavaScript(js) files.'''
+    try:
+        return open(js_file, "r").read()
+    except Exception as e:
+        print("An error occurred : %s\n" % e)
