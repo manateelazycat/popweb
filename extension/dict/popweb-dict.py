752678f21b63e2594e97a409cebe0cfe0f94881f
czqhurricnae <czqhurricane@gmail.com>
2023-02-10 01:15:08 +0800

    Update popweb-dict.py

--- extension/dict/popweb-dict.py
+++ extension/dict/popweb-dict.py
@@ -28,7 +28,7 @@ def pop_translate_window(popweb, module_name, x, y, x_offset, y_offset, frame_x,
     window_x, window_y = popweb.adjust_render_pos(x + x_offset, y + y_offset, x_offset, y_offset, window_width, window_height, frame_x, frame_y, frame_w, frame_h)
 
     web_window.loading_js_code = loading_js_code
-    web_window.js_file_code = read_js_content(js_file) or ""
+    web_window.js_file_code = read_js_content(js_file)
     web_window.js_file_code_args = js_file_code_args
     web_window.webview.load(QUrl(url))
     web_window.update_theme_mode()
