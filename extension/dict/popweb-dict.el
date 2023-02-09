e824d254886ac060f18cf46cbb77403e693dcf7f
czqhurricnae <czqhurricane@gmail.com>
2023-02-09 13:17:02 +0800

    Update popweb-dict.el

--- extension/dict/popweb-dict.el
+++ extension/dict/popweb-dict.el
@@ -193,11 +193,11 @@ Otherwise return word around point."
          (add-hook 'post-command-hook #',func-hide-after-move))
        )))
 
-(defun popweb-join-dirs (root dir file)
+(defun popweb-dict-join-dirs (root dir file)
   (file-name-concat root dir file))
 
 (defun popweb-dict-js-file-path (file)
-  (popweb-join-dirs (file-name-directory popweb-dict-module-path) "js" file))
+  (popweb-dict-join-dirs (file-name-directory popweb-dict-module-path) "js" file))
 
 (popweb-dict-create "bing"
                     "http://www.bing.com/dict/search?mkt=zh-cn&q=%s"
