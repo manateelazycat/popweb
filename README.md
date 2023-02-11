62eec104528478bf65f52b81a955a561bd278a48
xhcoding <xhcoding@163.com>
2023-02-11 21:53:33 +0800

    support import cookie from other browsers

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ I consider `popweb` to be a sister project, and a lightweight version of EAF tha
 ## Installation
 1. Install PyQt6: ```pip install PyQt6 PyQt6-Qt6 PyQt6-sip PyQt6-WebEngine PyQt6-WebEngine-Qt6```
 2. Install [python-epc](https://github.com/tkf/python-epc): ```pip install epc```
-3. Clone or download this repository (path of the folder is the `<path-to-popweb>` used below).
-4. In your `~/.emacs`, add the following lines:
+3. Install [browser_cookie3](https://github.com/borisbabic/browser_cookie3)(optional, for import cookies from browser): ```pip install browser_cookie3```
+4. Clone or download this repository (path of the folder is the `<path-to-popweb>` used below).
+5. In your `~/.emacs`, add the following lines:
 ```elisp
 (add-to-list 'load-path "<path-to-popweb>") ; add popweb to your load-path
 
@@ -38,6 +39,7 @@ I consider `popweb` to be a sister project, and a lightweight version of EAF tha
 * `M-x popweb-org-roam-link-show`: Preview Org-Roam ID link or footnote link at point.
 * `M-x popweb-org-roam-link-preview-select` Select one of all Org-Roam ID links and footnote links in a Org-mode buffer by ivy, then preview the content of link.
 * `M-x popweb-org-roam-node-preview-select` Select one Org-Roam node by ivy, then hit `C-M-m ivy-call` to preview the content of node without exit ivy windown.
+* `M-x popweb-import-browser-cookies` Import cookies for the specified domain name.
 ## Screenshots
 ### LaTeX Preview
 <p align="center">
--- popweb.py
+++ popweb.py
@@ -25,9 +25,9 @@ from PyQt6.QtWebEngineWidgets import QWebEngineView
 
 from PyQt6 import QtCore
 from PyQt6.QtGui import QColor
-from PyQt6.QtCore import QUrl, Qt, QEventLoop
-from PyQt6.QtNetwork import QNetworkProxy, QNetworkProxyFactory
-from PyQt6.QtWebEngineCore import QWebEnginePage, QWebEngineSettings
+from PyQt6.QtCore import QDateTime, QUrl, Qt, QEventLoop
+from PyQt6.QtNetwork import QNetworkCookie, QNetworkProxy, QNetworkProxyFactory
+from PyQt6.QtWebEngineCore import QWebEnginePage, QWebEngineSettings, QWebEngineProfile
 from PyQt6.QtWidgets import QWidget, QApplication, QVBoxLayout
 from epc.client import EPCClient
 from epc.server import ThreadingEPCServer
@@ -150,6 +150,188 @@ def get_emacs_func_result(method_name, args):
         result = epc_client.call_sync("eval-in-emacs", args)
         return result if result != [] else False
 
+emacs_config_dir = ""
+
+def get_emacs_config_dir():
+    import os
+
+    global emacs_config_dir
+
+    if emacs_config_dir == "":
+        emacs_config_dir = os.path.join(os.path.expanduser(get_emacs_var("popweb-config-location")), '')
+
+    if not os.path.exists(emacs_config_dir):
+        os.makedirs(emacs_config_dir)
+
+    return emacs_config_dir
+
+def touch(path):
+    import os
+
+    if not os.path.exists(path):
+        basedir = os.path.dirname(path)
+
+        if not os.path.exists(basedir):
+            os.makedirs(basedir)
+
+        with open(path, 'a'):
+            os.utime(path)
+
+class CookiesManager(object):
+
+    def __init__(self, browser_view):
+        self.browser_view = browser_view
+
+        self.cookies_dir = self.get_cookies_dir()
+
+        # Both session and persistent cookies are stored in memory
+        self.browser_view.page().profile().setPersistentCookiesPolicy(QWebEngineProfile.PersistentCookiesPolicy.NoPersistentCookies)
+
+        self.cookie_store = self.browser_view.page().profile().cookieStore()
+
+        self.cookie_store.cookieAdded.connect(self.add_cookie)      # save cookie to disk when captured cookieAdded signal
+        self.cookie_store.cookieRemoved.connect(self.remove_cookie) # remove cookie stored on disk when captured cookieRemoved signal
+        self.browser_view.loadStarted.connect(self.load_cookie)     # load disk cookie to QWebEngineView instance when page start load
+
+    @classmethod
+    def get_cookies_dir(cls):
+        return os.path.join(get_emacs_config_dir(), "browser", "cookies")
+
+    @classmethod
+    def add_cookie(cls, cookie):
+        '''Store cookie on disk.'''
+        cookie_domain = cookie.domain()
+        if not cookie.isSessionCookie():
+            cookie_file = os.path.join(cls.get_cookies_dir(), cookie_domain, cls._generate_cookie_filename(cookie))
+            touch(cookie_file)
+
+            # Save newest cookie to disk.
+            with open(cookie_file, "wb") as f:
+                f.write(cookie.toRawForm())
+
+    def load_cookie(self):
+        ''' Load cookie file from disk.'''
+        if not os.path.exists(self.cookies_dir):
+            return
+
+        all_cookies_domain = os.listdir(self.cookies_dir)
+        for domain in filter(self.domain_matching, all_cookies_domain):
+            from PyQt6.QtNetwork import QNetworkCookie
+
+            domain_dir = os.path.join(self.cookies_dir, domain)
+
+            for cookie_file in os.listdir(domain_dir):
+                with open(os.path.join(domain_dir, cookie_file), "rb") as f:
+                    for cookie in QNetworkCookie.parseCookies(f.read()):
+                        if not domain.startswith('.'):
+                            if self.browser_view.url().host() == domain:
+                                # restore host-only cookie
+                                cookie.setDomain('')
+                                self.cookie_store.setCookie(cookie, self.browser_view.url())
+                        else:
+                            self.cookie_store.setCookie(cookie)
+
+    def remove_cookie(self, cookie):
+        ''' Delete cookie file.'''
+        if not cookie.isSessionCookie():
+            cookie_file = os.path.join(self.cookies_dir, cookie.domain(), self._generate_cookie_filename(cookie))
+
+            if os.path.exists(cookie_file):
+                os.remove(cookie_file)
+
+    def delete_all_cookies(self):
+        ''' Simply delete all cookies stored on memory and disk.'''
+        self.cookie_store.deleteAllCookies()
+        if os.path.exists(self.cookies_dir):
+            import shutil
+            shutil.rmtree(self.cookies_dir)
+
+    def delete_cookie(self):
+        ''' Delete all cookie used by current site except session cookies.'''
+        from PyQt6.QtNetwork import QNetworkCookie
+        import shutil
+
+        cookies_domain = os.listdir(self.cookies_dir)
+
+        for domain in filter(self.get_relate_domains, cookies_domain):
+            domain_dir = os.path.join(self.cookies_dir, domain)
+
+            for cookie_file in os.listdir(domain_dir):
+                with open(os.path.join(domain_dir, cookie_file), "rb") as f:
+                    for cookie in QNetworkCookie.parseCookies(f.read()):
+                        self.cookie_store.deleteCookie(cookie)
+            shutil.rmtree(domain_dir)
+
+    def domain_matching(self, cookie_domain):
+        ''' Check if a given cookie's domain is matching for host string.'''
+        return True
+        cookie_is_hostOnly = True
+        if cookie_domain.startswith('.'):
+            # get rid of prefixing dot when matching domains
+            cookie_domain = cookie_domain[1:]
+            cookie_is_hostOnly = False
+
+        host_string = self.browser_view.url().host()
+        if cookie_domain == host_string:
+            # The domain string and the host string are identical
+            return True
+
+        if len(host_string) < len(cookie_domain):
+            # For obvious reasons, the host string cannot be a suffix if the domain
+            # is shorter than the domain string
+            return False
+
+        if host_string.endswith(cookie_domain) and host_string[:-len(cookie_domain)][-1] == '.' and not cookie_is_hostOnly:
+            # The domain string should be a suffix of the host string,
+            # The last character of the host string that is not included in the
+            # domain string should be a %x2E (".") character.
+            # and cookie domain not have prefixing dot (host-only cookie is not for subdomains)
+            return True
+
+        return False
+
+    def get_relate_domains(self, cookie_domain):
+        ''' Check whether the cookie domain is located under the same root host as the current URL host.'''
+        import tld, re
+
+        host_string = self.browser_view.url().host()
+
+        if cookie_domain.startswith('.'):
+            cookie_domain = cookie_domain[1:]
+
+        base_domain = tld.get_fld(host_string, fix_protocol=True, fail_silently=True)
+
+        if not base_domain:
+            # check whether host string is an IP address
+            if re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$').match(host_string) and host_string == cookie_domain:
+                return True
+            return  False
+
+        if cookie_domain == base_domain:
+            return True
+
+        if cookie_domain.endswith(base_domain) and cookie_domain[:-len(base_domain)][-1] == '.':
+            return True
+
+        return False
+
+    @classmethod
+    def _generate_cookie_filename(cls, cookie):
+        ''' Gets the name of the cookie file stored on the hard disk.'''
+        name = cookie.name().data().decode("utf-8")
+        domain = cookie.domain()
+        if os.name == "nt":
+            encode_path = cookie.path().encode("utf-8").hex()
+        else:
+            encode_path = cookie.path().replace("/", "|")
+
+        return name + "+" + domain + "+" + encode_path
+
+class WebView(QWebEngineView):
+    def __init__(self):
+        super(QWebEngineView, self).__init__()
+        self.cookies_manager = CookiesManager(self)
+
 class BrowserPage(QWebEnginePage):
     def __init__(self):
         QWebEnginePage.__init__(self)
@@ -199,8 +381,7 @@ class WebWindow(QWidget):
         self.dark_mode_js = open(os.path.join(os.path.dirname(__file__), "darkreader.js")).read()
 
         self.update_theme_mode()
-
-        self.webview = QWebEngineView()
+        self.webview = WebView()
         self.web_page = BrowserPage()
         self.webview.setPage(self.web_page)
         self.web_page.setBackgroundColor(QColor(get_emacs_func_result("popweb-get-theme-background", [])))
@@ -418,6 +599,25 @@ class POPWEB(object):
 
             if hasattr(web_window, "developer_tools_view"):
                 web_window.developer_tools_view.hide()
+
+    def import_browser_cookies(self, browser, domain_name):
+        try:
+            import browser_cookie3
+            cookiejar = getattr(browser_cookie3, browser)(domain_name=domain_name)
+            if cookiejar is None:
+                print("Import cookie failed!")
+                return
+
+            for cookie in cookiejar:
+                qt_cookie = QNetworkCookie(cookie.name.encode(), cookie.value.encode())
+                qt_cookie.setDomain(cookie.domain)
+                qt_cookie.setSecure(cookie.secure)
+                qt_cookie.setPath(cookie.path)
+                qt_cookie.setExpirationDate(QDateTime.fromSecsSinceEpoch(0 if cookie.expires is None else cookie.expires))
+                CookiesManager.add_cookie(qt_cookie)
+        except:
+            import traceback
+            traceback.print_exc()
 
     def cleanup(self):
         '''Do some cleanup before exit python process.'''
